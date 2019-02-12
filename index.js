f8200ce0113fb617380cd86e6437318fdfef1fb3
Anna Linkova <42319409+linkovanne@users.noreply.github.com>
2019-02-12 15:13:34 +0300

    Update index.js
    amicables found. additional sort by max value needed

--- index.js
+++ index.js
@@ -1,38 +1,78 @@
-let nb = +isPrime(7);
-
 function isPrime(nb) {
-    // let i = 2;
-    // if(nb <= 1){
-    //     return false;
-    // } else {
-    //     while (i*i <= nb){
-    //         if (nb % i == 0)
-    //         {
-    //             break;
-    //         }
-    //         i++;
-    //     }
-    //     return (nb);
-    // }
     if(nb <= 1){
-        return ;
+        return;
     } else if(nb == 2 || nb == 3){
         return(nb);
     } else {
         for(i = 2; i*i <= nb; i++){
             if (nb % i == 0){
-                return ;
+                return;
             }
         }
         return(nb);
     }
 }
-console.log(nb);
 
-let p = 3*(2**(nb-1)-1);
-let q = 3*(2**(nb)-1);
-let r = 9*(2**(nb-1)-1);
+function countP(n){
+    if (n <= 1) {
+        return;
+    } else {
+        let p = +isPrime(3 * (2 ** (n - 1)) - 1);
+        // let p = 3 * (2 ** (n - 1)) - 1;
+        return(p);
+    }
+}
+function countQ(n){
+    if (n <= 1) {
+        return;
+    } else {
+        let q = +isPrime(3 * (2 ** (n)) - 1);
+        // let q = 3 * (2 ** (n)) - 1;
+        return(q);
+    }
+}
+function countR(n){
+    if (n <= 1) {
+        return;
+    } else {
+        let r = +isPrime(9 * (2 ** (2 * n - 1)) - 1);
+        // let r = 9 * (2 ** (2*n - 1)) - 1;
+        return(r);
+    }
+}
 
-console.log(p);
-console.log(q);
-console.log(r);
+function firstAmicable(n, p, q) {
+    let fAm = (2 ** n) * p * q;
+    return (fAm);
+}
+
+function secondAmicable(n, r) {
+    let sAm = (2 ** n) * r;
+    return (sAm);
+}
+
+function writeArr(a, z) {
+    let arr= [];
+    let j = z - a + 1;
+    for (i = 0; i < j; i++){
+        arr[i] = a;
+        a++;
+    }
+    // return (arr);
+    arr.forEach(function (i) {
+        let l = countP(i);
+        let m = countQ(i);
+        let n = countR(i);
+        if(!(isNaN(l)) && !(isNaN(m)) && !(isNaN(n))){
+            console.log(firstAmicable(i, l, m));
+            console.log(secondAmicable(i, n));
+            // console.log(l);
+            // console.log(m);
+            // console.log(n);
+        } else  {
+            console.log(i + ' is not amicable');
+        }
+    });
+}
+
+console.log(writeArr(1, 10));

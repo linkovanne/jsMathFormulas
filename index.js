f82f9a3bab925c4f427abe13af2270b44dfbfb66
Anna Linkova <42319409+linkovanne@users.noreply.github.com>
2019-02-15 00:11:42 +0300

    amicable numbers

--- index.js
+++ index.js
@@ -1,43 +1,43 @@
 function isPrime(nb) {
-    if(nb <= 1){
+    if (nb <= 1) {
         return;
-    } else if(nb == 2 || nb == 3){
-        return(nb);
+    } else if (nb == 2 || nb == 3) {
+        return (nb);
     } else {
-        for(i = 2; i*i <= nb; i++){
-            if (nb % i == 0){
+        for (i = 2; i * i <= nb; i++) {
+            if (nb % i == 0) {
                 return;
             }
         }
-        return(nb);
+        return (nb);
     }
 }
 
-function countP(n){
+function countP(n) {
     if (n <= 1) {
         return;
     } else {
         let p = +isPrime(3 * (2 ** (n - 1)) - 1);
         // let p = 3 * (2 ** (n - 1)) - 1;
-        return(p);
+        return (p);
     }
 }
-function countQ(n){
+
+function countQ(n) {
     if (n <= 1) {
         return;
     } else {
         let q = +isPrime(3 * (2 ** (n)) - 1);
-        // let q = 3 * (2 ** (n)) - 1;
-        return(q);
+        return (q);
     }
 }
-function countR(n){
+
+function countR(n) {
     if (n <= 1) {
         return;
     } else {
         let r = +isPrime(9 * (2 ** (2 * n - 1)) - 1);
-        // let r = 9 * (2 ** (2*n - 1)) - 1;
-        return(r);
+        return (r);
     }
 }
 
@@ -51,28 +51,23 @@ function secondAmicable(n, r) {
     return (sAm);
 }
 
-function writeArr(a, z) {
-    let arr= [];
-    let j = z - a + 1;
-    for (i = 0; i < j; i++){
-        arr[i] = a;
-        a++;
-    }
-    // return (arr);
-    arr.forEach(function (i) {
-        let l = countP(i);
-        let m = countQ(i);
-        let n = countR(i);
+let findN = (a, z) => {
+    for (let i = 2; i < 100; i++){
+        let l = +countP(i);
+        let m = +countQ(i);
+        let n = +countR(i);
+
         if(!(isNaN(l)) && !(isNaN(m)) && !(isNaN(n))){
-            console.log(firstAmicable(i, l, m));
-            console.log(secondAmicable(i, n));
-            // console.log(l);
-            // console.log(m);
-            // console.log(n);
-        } else  {
-            console.log(i + ' is not amicable');
+            let fAmc = firstAmicable(i, l, m);
+            let sAmc = secondAmicable(i, n);
+            if (fAmc >= a && fAmc <= z && sAmc >= a && sAmc <= z){
+                let amicables = [fAmc, sAmc];
+                console.log(amicables)
+            } else {
+                break;
+            }
         }
-    });
+    }
 }
 
-console.log(writeArr(1, 10));
+findN(1, 500);
